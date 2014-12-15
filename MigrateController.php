b862fb7ebee328fd7facd044fa7f4f9ff0ba2330
uzproger <uzproger@gmail.com>
2014-12-15 17:16:26 +0800

    some fixes

--- MigrateController.php
+++ MigrateController.php
@@ -12,131 +12,130 @@ use yii\helpers\ArrayHelper;
  */
 class MigrateController extends BaseMigrateController
 {
-  /**
-   * @var string Main migration name
-   */
-  public $baseMigrationName = 'Base'; 
+    /**
+     * @var string Main migration name
+    */
+    public $baseMigrationName = 'Base';
   
-  /**
-   * @var array Migration pathes.
-   */
-  public $additionalPaths = [];
+    /**
+     * @var array Migration pathes.
+     */
+    public $additionalPaths = [];
 
-  /**
-   * TODO: Need make directory for additional paths if is not created yet
-   * @inheritdoc
-   */
-  public function beforeAction($action)
-  {
-    if (parent::beforeAction($action)) {
+    /**
+     * TODO: Need make directory for additional paths if is not created yet
+     * @inheritdoc
+     */
+    public function beforeAction($action)
+    {
+        echo "\n";
         $this->additionalPaths = ArrayHelper::merge([
-          [
-            'name' => $this->baseMigrationName,
-            'path' => $this->migrationPath,
-          ]  
+            [
+                'name' => $this->baseMigrationName,
+                'path' => $this->migrationPath,
+            ]  
         ], $this->additionalPaths);
-      $this->selectModule();
-      return true;
+        $this->selectModule();
+
+        return parent::beforeAction($action);
     }
-    return false;
-  }
   
-  /**
-   * Console select module
-   *
-   * @return void
-   */
-  protected function selectModule()
-  {
-    $selectedModule = Console::select("please select module", $this->getMigrationPaths());
-    $this->setMigrationPath($selectedModule);
-  }
-
-  /**
-   * Set migration path of selected module
-   *
-   * @throws Exception
-   * @return void
-   */
-  protected function setMigrationPath($module)
-  {
-    if (!isset($this->migrationPaths[$module]) || !isset($this->migrationPaths[$module]['path'])) {
-      throw new \Exception("Undefinied module or wrong path format.");   
+    /**
+     * Console select module
+     *
+     * @return void
+     */
+    protected function selectModule()
+    {
+        $selectedModule = Console::select("please select module", $this->getMigrationPaths());
+        $this->setMigrationPath($selectedModule);
     }
 
-    $this->migrationPath = $this->migrationPaths[$module]['path'];
-  }
+    /**
+     * Set migration path of selected module
+     *
+     * @throws Exception
+     * @return void
+     */
+    protected function setMigrationPath($module)
+    {
+        if (!isset($this->migrationPaths[$module]) || !isset($this->migrationPaths[$module]['path'])) {
+            throw new \Exception("Undefinied module or wrong path format.");   
+        }
 
-  /**
-   * @inheritdoc
-   */
-  protected function getMigrationHistory($limit)
-  {
-    if ($this->db->schema->getTableSchema($this->migrationTable, true) === null) {
-      $this->createMigrationHistoryTable();
+        $this->migrationPath = $this->migrationPaths[$module]['path'];
     }
-    $query = new Query;
-    $rows = $query->select(['version', 'apply_time'])
-      ->from($this->migrationTable)
-      ->where('path_hash=:path_hash', [':path_hash' => $this->hashPath()])
-      ->orderBy('version DESC')
-      ->limit($limit)
-      ->createCommand($this->db)
-      ->queryAll();
-    $history = ArrayHelper::map($rows, 'version', 'apply_time');
-    unset($history[self::BASE_MIGRATION]);
 
-    return $history;
-  }
+    /**
+     * @inheritdoc
+     */
+    protected function getMigrationHistory($limit)
+    {
+        if ($this->db->schema->getTableSchema($this->migrationTable, true) === null) {
+            $this->createMigrationHistoryTable();
+        }
+        $query = new Query;
+        $rows = $query->select(['version', 'apply_time'])
+            ->from($this->migrationTable)
+            ->where('path_hash=:path_hash', [':path_hash' => $this->hashPath()])
+            ->orderBy('version DESC')
+            ->limit($limit)
+            ->createCommand($this->db)
+            ->queryAll();
+        $history = ArrayHelper::map($rows, 'version', 'apply_time');
+        unset($history[self::BASE_MIGRATION]);
 
-  /**
-   * @inheritdoc
-   */
-  protected function createMigrationHistoryTable()
-  {
-    $tableName = $this->db->schema->getRawTableName($this->migrationTable);
-    echo "Creating migration history table \"$tableName\"...";
-    $this->db->createCommand()->createTable($this->migrationTable, [
-      'version' => 'varchar(180) NOT NULL PRIMARY KEY',
-      'path_hash' => 'varchar(32) NOT NULL',
-      'apply_time' => 'integer',
-    ])->execute();
-    $this->db->createCommand()->insert($this->migrationTable, [
-      'version' => self::BASE_MIGRATION,
-      'path_hash' => $this->hashPath(),
-      'apply_time' => time(),
-    ])->execute();
-    echo "done.\n";
-  }
+        return $history;
+    }
 
-  /**
-   * @inheritdoc
-   */
-  protected function addMigrationHistory($version)
-  {
-    $command = $this->db->createCommand();
-    $command->insert($this->migrationTable, [
-      'version' => $version,
-      'path_hash' => $this->hashPath(),
-      'apply_time' => time(),
-    ])->execute();
-  }
+    /**
+     * @inheritdoc
+     */
+    protected function createMigrationHistoryTable()
+    {
+        $tableName = $this->db->schema->getRawTableName($this->migrationTable);
+        echo "Creating migration history table \"$tableName\"...";
+        $this->db->createCommand()->createTable($this->migrationTable, [
+            'version' => 'varchar(180) NOT NULL PRIMARY KEY',
+            'path_hash' => 'varchar(32) NOT NULL',
+            'apply_time' => 'integer',
+        ])->execute();
+        $this->db->createCommand()->insert($this->migrationTable, [
+            'version' => self::BASE_MIGRATION,
+            'path_hash' => $this->hashPath(),
+            'apply_time' => time(),
+        ])->execute();
+        echo "done.\n";
+    }
 
-  /**
-   * Generate hash for current migration path
-   * @return string
-   */
-  protected function hashPath()
-  {
-    return md5($this->migrationPath);
-  } 
+    /**
+     * @inheritdoc
+     */
+    protected function addMigrationHistory($version)
+    {
+        $command = $this->db->createCommand();
+        $command->insert($this->migrationTable, [
+            'version' => $version,
+            'path_hash' => $this->hashPath(),
+            'apply_time' => time(),
+        ])->execute();
+    }
 
-  /**
-   * Returns migration paths
-   * @return array
-   */
-  protected function getMigrationPaths()
-  {
-    return $this->additionalPaths;
-  }
-}  
+    /**
+     * Generate hash for current migration path
+     * @return string
+     */
+    protected function hashPath()
+    {
+        return md5($this->migrationPath);
+    } 
+
+    /**
+     * Returns migration paths
+     * @return array
+     */
+    protected function getMigrationPaths()
+    {
+        return $this->additionalPaths;
+    }
+}
